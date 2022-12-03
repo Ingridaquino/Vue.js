ec3e9a5db0397d421532d5997a0812a220fbb889
Ingrid Aquino <ingridaquinosoares@gmail.com>
2022-12-03 10:01:07 -0300

    :construction: Created function localStorage

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const vm = new Vue({
   el: "#app",
   data : {
     produtos: [],
-    produto: true,
+    produto: false,
+    carrinho: [],    
   },
   filters:{
     numeroPreco(valor){
       return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})
+    }
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+      }
+      return total;
     }
   },
   methods: {
@@ -35,9 +47,32 @@ const vm = new Vue({
     fecharModal({target, currentTarget}){
       if(target === currentTarget)
       this.produto = false;
+    },
+    adicionarItem(){
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+      console.log(carrinho)
+    },
+    removerItem(index){
+      this.carrinho.splice(index, 1);
+      console.log(carrinho.lenght)
+    },
+    checarLocalStorage(){
+      if(window.localStorage.carrinho) {
+        //parse para transformar string em array
+        this.carrinho = JSON.parse(window.localStorage.carrinho)
+      }
+    }
+  },
+  watch: {
+    //salvar no localStorage
+    carrinho(){
+      window.localStorage.carrinho = JSON.stringify(this.carrinho)
     }
   },
   created(){
     this.fetchProdutos(); //ativa o fetch assim que é criado a pagina
+    this.checarLocalStorage();
   }
 })
